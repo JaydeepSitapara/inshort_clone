bf03805be43cd0acc0f60462d9e3ec0b36508b6d
Jaydeep Sitapara <jaydeepsitapara49@gmail.com>
2024-06-16 18:07:14 +0530

    inshort clone example

--- lib/common/constants.dart
+++ lib/common/constants.dart
@@ -1,8 +1,8 @@
 const String url = "newsapi.org";
 const String version = "v2";
-const String apiKey = "092f0bcc616b4f5b99810397dfc89382";
+const String apiKey = "";
 
-const String everything = "https://newsapi.org/v2/everything?q=keyword&apiKey=092f0bcc616b4f5b99810397dfc89382";
+const String everything = "https://newsapi.org/v2/everything?q=keyword&apiKey=";
 //const String everything = "$url/$version/everything?q=keyword&apiKey=$apiKey"; //for everything
 const String topHeadlinesUrl = "$url/$version/top-headlines?country=us&apiKey=$apiKey"; //for top-headlines
 
